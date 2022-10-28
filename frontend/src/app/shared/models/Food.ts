d6e344d9f50dae6229bb1be2406cbfd9dab22d1c
settler” <20CE155@charusat.edu.in>
2022-10-28 10:50:45 +0530

    done with food items

---
+++ frontend/src/app/shared/models/Food.ts
@@ -0,0 +1,15 @@
+export class Food{
+    id! : string;
+    name! : string;
+    price! : number;
+    tags? : string[];
+    favorite! : boolean;
+    stars! : number;
+    imageUrl! : string;
+    origins! : string[];
+    cookTime! : string;
+}
+export class Tag{
+    name! : string;
+    count! : number;
+}
